2dcfa6b270120b298b2fa755e63fcca6a43cb86d
roh68210 <roh4127@gmail.com>
2026-04-08 19:55:58 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "29719806"
+api_hash = "c8e87805739aa77bd5bd4076148a9a66"
+bot_token = "8347691106:AAGaBtR213QM7xu4DwHFxeEx269c_uSpr8I"
+auth_users = [1394117837]
+sudo_users = []
